5a6cfbc1c5e9e06a856fe5ae0fde9885ad01565d
Jake Duncan <Jaked222@users.noreply.github.com>
2023-12-25 12:56:01 -0600

    Add call button, remove sections

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,10 +14,10 @@ export const Header = (props) => {
                 </h1>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
                 <a
-                  href="#features"
+                  href="tel:5164272400"
                   className="btn btn-custom btn-lg page-scroll"
                 >
-                  Learn More
+                  Call Today
                 </a>{" "}
               </div>
             </div>
